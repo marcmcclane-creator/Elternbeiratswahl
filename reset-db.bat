b66d01e2f087df7dad4da5e63bd6d313242a3e86
marcmcclane-creator <marcmcclane@gmail.com>
2025-09-22 00:19:10 +0200

    remove plain DB password from reset-db.bat

--- reset-db.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starte Reset der Render-Datenbank...
-docker run --rm -i postgres psql "postgresql://elternbeiratswahl_db_user:REMOVED_SECRET@dpg-d3849uhr0fns73fedipg-a.frankfurt-postgres.render.com/elternbeiratswahl_db" < reset.sql
-echo Fertig! Datenbank wurde zurückgesetzt.
-pause
